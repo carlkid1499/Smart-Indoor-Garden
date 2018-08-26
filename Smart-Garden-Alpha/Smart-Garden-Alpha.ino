391534073ea77c80cce5f807a971ebf129dce957
Carlos <csantos27043@hotmail.com>
2018-08-25 22:32:36 -0700

    Added Alpha Test File
    Created file for initial goals

---
+++ Smart-Garden-Alpha/Smart-Garden-Alpha.ino
@@ -0,0 +1,18 @@
+/* First program for smart-garden.
+ *  Goals: (1) Grab time from RTC, water every 3 days for 10 seconds
+ *         (2) Water on command with Button, Reset counter for days          
+ */
+
+/* Libraries for RTC */
+#include <Wire.h>
+#include "RTClib.h"
+/* End of Linraries for RTC */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
