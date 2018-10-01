0c3becae4127bd969abb47364e59f10d2326ec2c
Carlos <csantos27043@hotmail.com>
2018-09-30 18:47:26 -0700

    Messages
    Added in on/off message.
    Cleaned up code a bit.

--- Smart_Garden_Beta/smart_garden_Beta.ino
+++ Smart_Garden_Beta/smart_garden_Beta.ino
@@ -7,8 +7,7 @@
 #define RELAY1  2
 
 //variables to hold the current status of the button.(LOW == unpressed, HIGH = pressed)
-int buttonState1 = 0;         
-int buttonState2 = 0;
+int buttonState1 = 0;
 
 // Date and time functions using a DS1307 RTC connected via I2C and Wire lib
 #include <Wire.h>
@@ -62,8 +61,49 @@ stopwater = (future + TimeSpan(0,0,0,10));
 
 void loop () 
 {
-/* Code for the RTC Unit */
-    DateTime now = rtc.now(); // grabs current time
+/* Begin code for Relay */
+  Water_On();
+  Water_Off();
+/* End code for Relay */
+}
+
+DateTime Get_Future_Time() //  Funtion to grab future time 3 days
+{
+    DateTime now = rtc.now();
+    DateTime future (now + TimeSpan(0,0,0,10));
+    Serial.println("Message: Future time grabbed");
+    Serial.print(future.year(), DEC);
+    Serial.print('/');
+    Serial.print(future.month(), DEC);
+    Serial.print('/');
+    Serial.print(future.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[future.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(future.hour(), DEC);
+    Serial.print(':');
+    Serial.print(future.minute(), DEC);
+    Serial.print(':');
+    Serial.print(future.second(), DEC);
+    Serial.println();
+    Serial.println("End of  Message");
+    Serial.println();
+    Serial.println();
+    
+    return future;
+}
+
+void Water_On() // check current time against future time
+{
+  DateTime now = rtc.now();
+  if( (now.year() == future.year()) && (now.month() == future.month()) &&
+  (now.day() == future.day()) && (now.hour() == future.hour()) && 
+  (now.minute() == future.minute()) && (now.second() == future.second()) )
+  {
+    int buttonState1 = 1;
+    digitalWrite(RELAY1,buttonState1);
+    
+    Serial.println("Message: Water pump on");
     Serial.print(now.year(), DEC);
     Serial.print('/');
     Serial.print(now.month(), DEC);
@@ -78,35 +118,11 @@ void loop ()
     Serial.print(':');
     Serial.print(now.second(), DEC);
     Serial.println();
+    Serial.println("End of  Message");
     Serial.println();
-    delay(1000);
-/* End of code for the RTC Unit */
-/* Begin code for Relay */
-  Water_On();
-  Water_Off();
-/* End code for Relay */
-
-}
-
-DateTime Get_Future_Time() //  Funtion to grab future time 3 days
-{
-    DateTime now = rtc.now();
-    DateTime future (now + TimeSpan(0,0,0,10));
-    Serial.println("Message: Future time grabbed");
-    
-    return future;
-}
-
-void Water_On() // check current time against future time
-{
-  DateTime now = rtc.now();
-  if( (now.year() == future.year()) && (now.month() == future.month()) &&
-  (now.day() == future.day()) && (now.hour() == future.hour()) && 
-  (now.minute() == future.minute()) && (now.second() == future.second()) )
-  {
-    digitalWrite(RELAY1,1);
-    Serial.println("Message: Water pump on");
-  }
+    Serial.println();
+ 
+  } 
 }
 
 void Water_Off() // checks current time against stop time
@@ -120,9 +136,41 @@ void Water_Off() // checks current time against stop time
     Serial.println("Message: Water pump off");
     Serial.println("Message: Setting new future time");
     future = Get_Future_Time();
-   
+
+    Serial.print(future.year(), DEC);
+    Serial.print('/');
+    Serial.print(future.month(), DEC);
+    Serial.print('/');
+    Serial.print(future.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[future.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(future.hour(), DEC);
+    Serial.print(':');
+    Serial.print(future.minute(), DEC);
+    Serial.print(':');
+    Serial.print(future.second(), DEC);
+
+    Serial.println(); 
     Serial.println("Message: Seting new stopwater time");
     stopwater = (future + TimeSpan(0,0,0,10));
-  
+
+    Serial.print(stopwater.year(), DEC);
+    Serial.print('/');
+    Serial.print(stopwater.month(), DEC);
+    Serial.print('/');
+    Serial.print(stopwater.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[stopwater.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(stopwater.hour(), DEC);
+    Serial.print(':');
+    Serial.print(stopwater.minute(), DEC);
+    Serial.print(':');
+    Serial.print(stopwater.second(), DEC);
+    Serial.println();
+    Serial.println("End of  Message");
+    Serial.println();
+    Serial.println();
   }
 }
