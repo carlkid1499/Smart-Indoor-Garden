32f90df65303bb1d0c1cdac4744af17906ac398c
Carlos <csantos27043@hotmail.com>
2018-10-28 13:26:18 -0700

    Alpha
    Moving test Alpha code to Beta Branch.

--- Smart_Garden_Beta/smart_garden_Beta.ino
+++ Smart_Garden_Beta/smart_garden_Beta.ino
@@ -1,13 +1,8 @@
-/* Smart Garden Project */
-/* 1 Relay Switch to control pumop */
-/* RTC added on to keep track of time */
-
-// use #define to set the I/O numbers, since these will never change - this saves us memory while the Arduino is running
-#define BUTTON1 1
-#define RELAY1  2
-
-//variables to hold the current status of the button.(LOW == unpressed, HIGH = pressed)
-int buttonState1 = 0;
+/* Smart Garden Project
+ * 1 Relay Switch to control pumop
+ * RTC added on to keep track of time 
+ * DIO 1: Push Button DIO 2: Relay, DIO 3: LED 
+ */
 
 // Date and time functions using a DS1307 RTC connected via I2C and Wire lib
 #include <Wire.h>
@@ -15,62 +10,60 @@ int buttonState1 = 0;
 
 #if defined(ARDUINO_ARCH_SAMD)
 // for Zero, output on USB Serial console, remove line below if using programming port to program the Zero!
-   #define Serial SerialUSB
+#define Serial SerialUSB
 #endif
 
 RTC_PCF8523 rtc;
 
+// use #define to set the I/O numbers, since these will never change - this saves us memory while the Arduino is running
+#define BUTTON_1 1
+#define RELAY_1  2
+#define LED_1 3
+
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-DateTime Get_Future_Time();
-void Water_On();
-void Water_Off();
 DateTime future;
 DateTime stopwater;
+//bool OnOff = false;
+
 void setup () 
 {
-/* Code for relay switch and pump */
-
-   // set the pin for the Buzzer as output:
-  pinMode(BUTTON1, INPUT);          
-
-  // initialize the pins for the pushbutton as inputs:
-  pinMode(RELAY1, OUTPUT);   
-/* End of code for relay switch and pump */ 
-
-/* Code for the RTC Unit */
-#ifndef ESP8266
-  while (!Serial); // for Leonardo/Micro/Zero
-#endif
-
+  /* Code for the RTC Unit */
   Serial.begin(57600);
-  if (! rtc.begin()) {
+  if (! rtc.begin()) 
+  {
     Serial.println("Couldn't find RTC");
     while (1);
   }
 
-  if (! rtc.initialized()) {
+  if (! rtc.initialized()) 
+  {
     Serial.println("RTC is NOT running!");
     // following line sets the RTC to the date & time this sketch was compiled
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
-/* End of code for the RTC Unit */
+  /* End of code for the RTC Unit */
 
-future = Get_Future_Time();
-stopwater = (future + TimeSpan(0,0,0,10));
+  future = Get_Future_Time(); // grab future data
+  stopwater = (future + TimeSpan(0,0,0,15)); // set water time
+
+  /* Code for relay switch, pump,button, and LED */
+  pinMode(LED_1, OUTPUT);
+  pinMode(BUTTON_1, INPUT);          
+  pinMode(RELAY_1, OUTPUT);   
+  /* End of code for relay switch, pump,button, and LED */ 
+
 }
 
 void loop () 
 {
-/* Begin code for Relay */
   Water_On();
   Water_Off();
-/* End code for Relay */
 }
 
 DateTime Get_Future_Time() //  Funtion to grab future time 3 days
 {
     DateTime now = rtc.now();
-    DateTime future (now + TimeSpan(0,0,0,10));
+    DateTime future (now + TimeSpan(3,0,0,0)); // 3 days
     Serial.println("Message: Future time grabbed");
     Serial.print(future.year(), DEC);
     Serial.print('/');
@@ -86,10 +79,6 @@ DateTime Get_Future_Time() //  Funtion to grab future time 3 days
     Serial.print(':');
     Serial.print(future.second(), DEC);
     Serial.println();
-    Serial.println("End of  Message");
-    Serial.println();
-    Serial.println();
-    
     return future;
 }
 
@@ -100,29 +89,11 @@ void Water_On() // check current time against future time
   (now.day() == future.day()) && (now.hour() == future.hour()) && 
   (now.minute() == future.minute()) && (now.second() == future.second()) )
   {
-    int buttonState1 = 1;
-    digitalWrite(RELAY1,buttonState1);
-    
+    digitalWrite(RELAY_1,1);
     Serial.println("Message: Water pump on");
-    Serial.print(now.year(), DEC);
-    Serial.print('/');
-    Serial.print(now.month(), DEC);
-    Serial.print('/');
-    Serial.print(now.day(), DEC);
-    Serial.print(" (");
-    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-    Serial.print(") ");
-    Serial.print(now.hour(), DEC);
-    Serial.print(':');
-    Serial.print(now.minute(), DEC);
-    Serial.print(':');
-    Serial.print(now.second(), DEC);
-    Serial.println();
-    Serial.println("End of  Message");
-    Serial.println();
-    Serial.println();
- 
-  } 
+    //OnOff = true;
+    Water_On_LED();
+  }
 }
 
 void Water_Off() // checks current time against stop time
@@ -132,36 +103,19 @@ void Water_Off() // checks current time against stop time
     (now.day() == stopwater.day()) && (now.hour() == stopwater.hour()) && 
     (now.minute() == stopwater.minute()) && (now.second() == stopwater.second()))
   {
-    digitalWrite(RELAY1,0);
+    digitalWrite(RELAY_1,0);
     Serial.println("Message: Water pump off");
     Serial.println("Message: Setting new future time");
     future = Get_Future_Time();
-
-    Serial.print(future.year(), DEC);
-    Serial.print('/');
-    Serial.print(future.month(), DEC);
-    Serial.print('/');
-    Serial.print(future.day(), DEC);
-    Serial.print(" (");
-    Serial.print(daysOfTheWeek[future.dayOfTheWeek()]);
-    Serial.print(") ");
-    Serial.print(future.hour(), DEC);
-    Serial.print(':');
-    Serial.print(future.minute(), DEC);
-    Serial.print(':');
-    Serial.print(future.second(), DEC);
-
-    Serial.println(); 
     Serial.println("Message: Seting new stopwater time");
-    stopwater = (future + TimeSpan(0,0,0,10));
-
+    stopwater = (future + TimeSpan(0,0,0,15));
     Serial.print(stopwater.year(), DEC);
     Serial.print('/');
     Serial.print(stopwater.month(), DEC);
     Serial.print('/');
     Serial.print(stopwater.day(), DEC);
     Serial.print(" (");
-    Serial.print(daysOfTheWeek[stopwater.dayOfTheWeek()]);
+    Serial.print(daysOfTheWeek[future.dayOfTheWeek()]);
     Serial.print(") ");
     Serial.print(stopwater.hour(), DEC);
     Serial.print(':');
@@ -169,8 +123,23 @@ void Water_Off() // checks current time against stop time
     Serial.print(':');
     Serial.print(stopwater.second(), DEC);
     Serial.println();
-    Serial.println("End of  Message");
-    Serial.println();
-    Serial.println();
+    //OnOff = false;
   }
+}
+
+void Water_On_LED()
+{
+    // blink for water on
+    digitalWrite(LED_1, HIGH);
+    delay(100);                      
+    digitalWrite(LED_1, LOW);
+    delay(100);
+    digitalWrite(LED_1, HIGH);
+    delay(100);                    
+    digitalWrite(LED_1, LOW); 
+    delay(100);                       
+    digitalWrite(LED_1, HIGH);  
+    delay(1000);                       
+    digitalWrite(LED_1, LOW);  
+    delay(1000);
 }
