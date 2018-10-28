ef5ebb900f50c8a51d43d20b4c9af4be2173257a
Carlos <csantos27043@hotmail.com>
2018-10-28 13:24:55 -0700

    LED
    Fixed LED issue... DIO 0 was not providing enough power... swapped to DIO 3

--- smart_garden_Alpha/smart_garden_Alpha.ino
+++ smart_garden_Alpha/smart_garden_Alpha.ino
@@ -1,7 +1,7 @@
 /* Smart Garden Project
  * 1 Relay Switch to control pumop
  * RTC added on to keep track of time 
- * DIO 0: LED, DIO 1: Push Button DIO 2: Relay
+ * DIO 1: Push Button DIO 2: Relay, DIO 3: LED 
  */
 
 // Date and time functions using a DS1307 RTC connected via I2C and Wire lib
@@ -16,30 +16,18 @@
 RTC_PCF8523 rtc;
 
 // use #define to set the I/O numbers, since these will never change - this saves us memory while the Arduino is running
-#define LED_1    0
 #define BUTTON_1 1
 #define RELAY_1  2
+#define LED_1 3
 
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 DateTime future;
 DateTime stopwater;
-bool OnOff;s
+//bool OnOff = false;
 
 void setup () 
 {
-  // LED
-  #define LED_1 0
-  pinMode(LED_1, OUTPUT);
-  /* Code for relay switch, pump, and button */
-  pinMode(BUTTON_1, INPUT);          
-  pinMode(RELAY_1, OUTPUT);   
-  /* End of code for relay switch, pump, and button */ 
-
   /* Code for the RTC Unit */
-  #ifndef ESP8266
-    while (!Serial); // for Leonardo/Micro/Zero
-  #endif
-
   Serial.begin(57600);
   if (! rtc.begin()) 
   {
@@ -55,19 +43,27 @@ void setup ()
   }
   /* End of code for the RTC Unit */
 
-  future = Get_Future_Time();
-  stopwater = (future + TimeSpan(0,0,0,5));
+  future = Get_Future_Time(); // grab future data
+  stopwater = (future + TimeSpan(0,0,0,15)); // set water time
+
+  /* Code for relay switch, pump,button, and LED */
+  pinMode(LED_1, OUTPUT);
+  pinMode(BUTTON_1, INPUT);          
+  pinMode(RELAY_1, OUTPUT);   
+  /* End of code for relay switch, pump,button, and LED */ 
+
 }
 
 void loop () 
 {
-  Check_Water();
+  Water_On();
+  Water_Off();
 }
 
 DateTime Get_Future_Time() //  Funtion to grab future time 3 days
 {
     DateTime now = rtc.now();
-    DateTime future (now + TimeSpan(0,0,1,0));
+    DateTime future (now + TimeSpan(3,0,0,0)); // 3 days
     Serial.println("Message: Future time grabbed");
     Serial.print(future.year(), DEC);
     Serial.print('/');
@@ -82,42 +78,68 @@ DateTime Get_Future_Time() //  Funtion to grab future time 3 days
     Serial.print(future.minute(), DEC);
     Serial.print(':');
     Serial.print(future.second(), DEC);
+    Serial.println();
     return future;
-}
-
-void Check_Water()
-{
-  DateTime now = rtc.now();
-  /* Begin code for Relay */
-  if( (now.year() == future.year()) && (now.month() == future.month()) &&
-  (now.day() == future.day()) && (now.hour() == future.hour()) && 
-  (now.minute() == future.minute()) && (now.second() == future.second()) )
-  {
-  Water_On();
-  }
-  if((now.year() == stopwater.year()) && (now.month() == stopwater.month()) &&
-    (now.day() == stopwater.day()) && (now.hour() == stopwater.hour()) && 
-    (now.minute() == stopwater.minute()) && (now.second() == stopwater.second()))
-  {
-  Water_Off();
-  }
-  /* End code for Relay */ 
 }
 
 void Water_On() // check current time against future time
 {
-    Serial.println();
-    Serial.println("Message: Water pump on");
+  DateTime now = rtc.now();
+  if( (now.year() == future.year()) && (now.month() == future.month()) &&
+  (now.day() == future.day()) && (now.hour() == future.hour()) && 
+  (now.minute() == future.minute()) && (now.second() == future.second()) )
+  {
     digitalWrite(RELAY_1,1);
+    Serial.println("Message: Water pump on");
+    //OnOff = true;
+    Water_On_LED();
+  }
 }
 
 void Water_Off() // checks current time against stop time
 {
+  DateTime now = rtc.now();
+  if((now.year() == stopwater.year()) && (now.month() == stopwater.month()) &&
+    (now.day() == stopwater.day()) && (now.hour() == stopwater.hour()) && 
+    (now.minute() == stopwater.minute()) && (now.second() == stopwater.second()))
+  {
     digitalWrite(RELAY_1,0);
     Serial.println("Message: Water pump off");
     Serial.println("Message: Setting new future time");
     future = Get_Future_Time();
-    Serial.println();
     Serial.println("Message: Seting new stopwater time");
-    stopwater = (future + TimeSpan(0,0,0,5));
+    stopwater = (future + TimeSpan(0,0,0,15));
+    Serial.print(stopwater.year(), DEC);
+    Serial.print('/');
+    Serial.print(stopwater.month(), DEC);
+    Serial.print('/');
+    Serial.print(stopwater.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[future.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(stopwater.hour(), DEC);
+    Serial.print(':');
+    Serial.print(stopwater.minute(), DEC);
+    Serial.print(':');
+    Serial.print(stopwater.second(), DEC);
+    Serial.println();
+    //OnOff = false;
+  }
+}
+
+void Water_On_LED()
+{
+    // blink for water on
+    digitalWrite(LED_1, HIGH);
+    delay(100);                      
+    digitalWrite(LED_1, LOW);
+    delay(100);
+    digitalWrite(LED_1, HIGH);
+    delay(100);                    
+    digitalWrite(LED_1, LOW); 
+    delay(100);                       
+    digitalWrite(LED_1, HIGH);  
+    delay(1000);                       
+    digitalWrite(LED_1, LOW);  
+    delay(1000);
 }
