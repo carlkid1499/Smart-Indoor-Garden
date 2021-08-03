e1ec069331d08b822391cac04ea22664f3762d05
Carlos <carlos.santos112@outlook.com>
2021-08-02 21:39:27 -0700

    Webpage
    got basic webpage up with buttons and text boxes

--- smart_garden/src/smart_garden.cpp
+++ smart_garden/src/smart_garden.cpp
@@ -1,213 +1,250 @@
 /* Smart Garden Project
 ** https://github.com/carlkid1499/Smart-Garden  
 ** 2 Relay switches to control 2 pumps
-** Adalogger FeatherWing - RTC + SD Add-on For All Feather Boards
 ** Adafruit HUZZAH32 – ESP32 Feather Board
 */
 
-/* ----- Begin: Import libs ----- */
+/*********
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp32-esp8266-input-data-html-form/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*********/
+
 #include <Arduino.h>
 #include <WiFi.h>
-#include <WiFiClient.h>
-#include <WebServer.h>
-#include <ESPmDNS.h>
-#include <RTClib.h>
-/* ----- End: Import Libs ----- */
+#include <AsyncTCP.h>
+#include <SPIFFS.h>
+#include <ESPAsyncWebServer.h>
 
-/* ----- Begin: Define Funcs/Tasks ----- */
-void setup();
-void loop();
-void SD_TASK();
-void RTC_TASK();
-void WEB_SERVER_TASK();
-void static handleRoot();
-void static handleNotFound();
-void static drawGraph();
-/* -----  End: Define Funcs/Tasks ----- */
+AsyncWebServer server(80);
 
-/* Begin: Define Global Vars -----*/
-RTC_PCF8523 rtc;
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-WebServer server(80);
-const int led = LED_BUILTIN;
-/* End: Define Global Vars -----*/
+// REPLACE WITH YOUR NETWORK CREDENTIALS
+const char *ssid = "";
+const char *password = "";
+
+// HTML web page to handle 3 input fields (inputString, inputInt, inputFloat)
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html><head>
+  <title>Smart Garden Dashboard</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script>
+    function submitMessage() {
+      alert("Saving value to ESP SPIFFS");
+      setTimeout(function(){ document.location.reload(false); }, 500);   
+    }
+  </script></head><body>
+  <form action="/get" target="hidden-form">
+    Set Light Time On: <input type="text" name="SetLightTimeOn">
+    <input type="submit" value="Submit" onclick="submitMessage()">
+  </form><br>
+  <form action="/get" target="hidden-form">
+    Set Light Time Off: <input type="text" name="SetLightTimeOff">
+    <input type="submit" value="Submit" onclick="submitMessage()">
+  </form><br>
+  <form action="/get" target="hidden-form">
+    Set Water Time On: <input type="text" name="SetWaterTimeOn">
+    <input type="submit" value="Submit" onclick="submitMessage()">
+  </form><br>
+  <form action="/get" target="hidden-form">
+    Set Water Time Off: <input type="text" name="SetWaterTimeOff">
+    <input type="submit" value="Submit" onclick="submitMessage()">
+  </form><br>
+  <form action="/get" target="hidden-form">
+    <input type="submit" name="BTN_WaterOn" value="Water On">
+    <input type="submit" name="BTN_WaterOff" value="Water Off">
+  </form><br>
+  <form action="/get" target="hidden-form">
+    <input type="submit" name="BTN_LightsOn" value="Lights On">
+    <input type="submit" name="BTN_LightsOff" value="Lights Off">
+  </form><br>
+  <iframe style="display:none" name="hidden-form"></iframe>
+</body></html>)rawliteral";
+
+void notFound(AsyncWebServerRequest *request)
+{
+  request->send(404, "text/plain", "Not found");
+}
+
+String readFile(fs::FS &fs, const char *path)
+{
+  Serial.printf("Reading file: %s\r\n", path);
+  File file = fs.open(path, "r");
+  if (!file || file.isDirectory())
+  {
+    Serial.println("- empty file or failed to open file");
+    return String();
+  }
+  Serial.println("- read from file:");
+  String fileContent;
+  while (file.available())
+  {
+    fileContent += String((char)file.read());
+  }
+  file.close();
+  Serial.println(fileContent);
+  return fileContent;
+}
+
+void writeFile(fs::FS &fs, const char *path, const char *message)
+{
+  Serial.printf("Writing file: %s\r\n", path);
+  File file = fs.open(path, "w");
+  if (!file)
+  {
+    Serial.println("- failed to open file for writing");
+    return;
+  }
+  if (file.print(message))
+  {
+    Serial.println("- file written");
+  }
+  else
+  {
+    Serial.println("- write failed");
+  }
+  file.close();
+}
+
+// Replaces placeholder with stored values
+String processor(const String &var)
+{
+  Serial.println(var);
+  if (var == "SetLightTimeOn")
+  {
+    return readFile(SPIFFS, "/config.json");
+  }
+  else if (var == "SetLightTimeOff")
+  {
+    return readFile(SPIFFS, "/config.json");
+  }
+  else if (var == "SetWaterTimeOn")
+  {
+    return readFile(SPIFFS, "/config.json");
+  }
+  else if (var == "SetWaterTimeOff")
+  {
+    return readFile(SPIFFS, "/config.json");
+  }
+  else if (var == "BTN_WaterOn")
+  {
+    return readFile(SPIFFS, "/config.json");
+  }
+  else if (var == "BTN_WaterOff")
+  {
+    return readFile(SPIFFS, "/config.json");
+  }
+  else if (var == "BTN_LightsOn")
+  {
+    return readFile(SPIFFS, "/config.json");
+  }
+  else if (var == "BTN_LightsOff")
+  {
+    return readFile(SPIFFS, "/config.json");
+  }
+  return String();
+}
 
 void setup()
 {
   Serial.begin(115200);
-
-  /* ----- Begin: Init the RTC ----- 
-  if (!rtc.begin())
+// Initialize SPIFFS
+#ifdef ESP32
+  if (!SPIFFS.begin(true))
   {
-    Serial.println("Couldn't find RTC!");
-    Serial.flush();
-    ESP.restart();
-    
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
   }
-  else{
-    if (!rtc.initialized() || rtc.lostPower())
+#else
+  if (!SPIFFS.begin())
   {
-    Serial.println("RTC is NOT initialized, let's set the time!");
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); // adjust to compile time
-    rtc.start();
-    DateTime now = rtc.now();
-    Serial.print(now.year(), DEC);
-    Serial.print('/');
-    Serial.print(now.month(), DEC);
-    Serial.print('/');
-    Serial.print(now.day(), DEC);
-    Serial.print(" (");
-    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-    Serial.print(") ");
-    Serial.print(now.hour(), DEC);
-    Serial.print(':');
-    Serial.print(now.minute(), DEC);
-    Serial.print(':');
-    Serial.print(now.second(), DEC);
-    Serial.println();
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    return;
   }
-    
-  }
-  
+#endif
 
-  -----  End: Init the RTC -----*/
-
-  /* ----- Begin: Web Server Init ----- */
-  const char *ssid = "";
-  const char *password = "";
-  pinMode(led, OUTPUT);
-  digitalWrite(led, 0);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  Serial.println("");
-
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED)
+  if (WiFi.waitForConnectResult() != WL_CONNECTED)
   {
-    delay(500);
-    Serial.print(".");
+    Serial.println("WiFi Failed!");
+    return;
   }
-
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
+  Serial.println();
+  Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 
-  if (MDNS.begin("smart_garden"))
-  {
-    Serial.println("MDNS responder started");
-  }
+  // Send web page with input fields to client
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send_P(200, "text/html", index_html, processor); });
 
-  server.on("/", handleRoot);
-  server.on("/test.svg", drawGraph);
-  server.on("/inline", []()
-            { server.send(200, "text/plain", "this works as well"); });
-  server.onNotFound(handleNotFound);
+  // Send a GET request to <ESP_IP>/get?inputString=<inputMessage>
+  server.on("/get", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+              String inputMessage;
+              // GET inputString value on <ESP_IP>/get?inputString=<inputMessage>
+              if (request->hasParam("SetLightTimeOn"))
+              {
+                inputMessage = request->getParam("SetLightTimeOn")->value();
+                writeFile(SPIFFS, "/config.json", inputMessage.c_str());
+              }
+              // GET inputInt value on <ESP_IP>/get?inputInt=<inputMessage>
+              else if (request->hasParam("SetLightTimeOff"))
+              {
+                inputMessage = request->getParam("SetLightTimeOff")->value();
+                writeFile(SPIFFS, "/config.json", inputMessage.c_str());
+              }
+              // GET inputFloat value on <ESP_IP>/get?inputFloat=<inputMessage>
+              else if (request->hasParam("SetWaterTimeOn"))
+              {
+                inputMessage = request->getParam("SetWaterTimeOn")->value();
+                writeFile(SPIFFS, "/config.json", inputMessage.c_str());
+              }
+              else if (request->hasParam("SetWaterTimeOff"))
+              {
+                inputMessage = request->getParam("SetWaterTimeOff")->value();
+                writeFile(SPIFFS, "/config.json", inputMessage.c_str());
+              }
+              else if (request->hasParam("BTN_LightsOn"))
+              {
+                inputMessage = request->getParam("BTN_LightsOn")->value();
+                writeFile(SPIFFS, "/config.json", "True");
+              }
+              else if (request->hasParam("BTN_LightsOff"))
+              {
+                inputMessage = request->getParam("BTN_LightsOff")->value();
+                writeFile(SPIFFS, "/config.json", "False");
+              }
+              else if (request->hasParam("BTN_WaterOn"))
+              {
+                inputMessage = request->getParam("BTN_WaterOn")->value();
+                writeFile(SPIFFS, "/config.json", "True");
+              }
+              else if (request->hasParam("BTN_WaterOff"))
+              {
+                inputMessage = request->getParam("BTN_WaterOff")->value();
+                writeFile(SPIFFS, "/config.json", "False");
+              }
+              else
+              {
+                inputMessage = "No message sent";
+              }
+              request->send(200, "text/text", inputMessage);
+            });
+  server.onNotFound(notFound);
   server.begin();
-  Serial.println("HTTP server started");
-  /* ----- End: Web Server Init ----- */
-}
-
-void SD_TASK()
-{
-}
-
-void RTC_TASK()
-{
-  DateTime now = rtc.now();
-  Serial.print(now.year(), DEC);
-  Serial.print('/');
-  Serial.print(now.month(), DEC);
-  Serial.print('/');
-  Serial.print(now.day(), DEC);
-  Serial.print(" (");
-  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-  Serial.print(") ");
-  Serial.print(now.hour(), DEC);
-  Serial.print(':');
-  Serial.print(now.minute(), DEC);
-  Serial.print(':');
-  Serial.print(now.second(), DEC);
-  Serial.println();
-  delay(3000);
-}
-
-void WEB_SERVER_TASK()
-{
-}
-
-void handleRoot()
-{
-  digitalWrite(led, 1);
-  char temp[400];
-  int sec = millis() / 1000;
-  int min = sec / 60;
-  int hr = min / 60;
-
-  snprintf(temp, 400,
-
-           "<html>\
-  <head>\
-    <meta http-equiv='refresh' content='5'/>\
-    <title>ESP32 Demo</title>\
-    <style>\
-      body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
-    </style>\
-  </head>\
-  <body>\
-    <h1>Hello from ESP32!</h1>\
-    <p>Uptime: %02d:%02d:%02d</p>\
-    <img src=\"/test.svg\" />\
-  </body>\
-</html>",
-
-           hr, min % 60, sec % 60);
-  server.send(200, "text/html", temp);
-  digitalWrite(led, 0);
-}
-
-void handleNotFound()
-{
-  digitalWrite(led, 1);
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-
-  for (uint8_t i = 0; i < server.args(); i++)
-  {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-
-  server.send(404, "text/plain", message);
-  digitalWrite(led, 0);
-}
-
-void drawGraph() {
-  String out = "";
-  char temp[100];
-  out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"400\" height=\"150\">\n";
-  out += "<rect width=\"400\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"1\" stroke=\"rgb(0, 0, 0)\" />\n";
-  out += "<g stroke=\"black\">\n";
-  int y = rand() % 130;
-  for (int x = 10; x < 390; x += 10) {
-    int y2 = rand() % 130;
-    sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - y, x + 10, 140 - y2);
-    out += temp;
-    y = y2;
-  }
-  out += "</g>\n</svg>\n";
-
-  server.send(200, "image/svg+xml", out);
 }
 
 void loop()
 {
-  server.handleClient();
-  delay(2);//allow the cpu to switch to other tasks
+  // To access your stored values on inputString, inputInt, inputFloat
+  //String yourInputString = readFile(SPIFFS, "/config.json");
+  //Serial.print("*** Your inputString: ");
+  // Serial.println(yourInputString);
+
+  delay(5000);
 }
